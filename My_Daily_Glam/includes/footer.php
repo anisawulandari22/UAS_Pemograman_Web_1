d6bd571b362da71cbf72ab72c08a5f4596240f85
anisawulandari22 <awul033@gmail.com>
2026-01-13 15:02:16 +0700

    Create footer.php

---
+++ My_Daily_Glam/includes/footer.php
@@ -0,0 +1,9 @@
+<body 
+style="position: relative; min-height: 100vh; padding-bottom: 50px;">
+    <footer class="custom-footer">
+        @Copyright by 23552011318_Anisa Wulandari_TIF RP 23 CNS A_UASWEB1
+    </footer>
+
+   <script src="bootstrap-5.3.8-dist/css/bootstrap.min.css"></script>
+</body>
+</html>
